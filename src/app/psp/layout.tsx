372b70b01502472edcae163d83748127f06e2a7f
mzakaria-creater <minafx2@gmail.com>
2026-06-11 19:12:55 +0300

    Add PSP platform layout

---
+++ src/app/psp/layout.tsx
@@ -0,0 +1,5 @@
+import { PlatformShell } from '@/components/psp/PlatformShell'
+
+export default function PSPLayout({ children }: { children: React.ReactNode }) {
+  return <PlatformShell>{children}</PlatformShell>
+}
