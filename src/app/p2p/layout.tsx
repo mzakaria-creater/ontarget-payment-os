6187d56483ebec48d1cdca704329556bb7805519
mzakaria-creater <minafx2@gmail.com>
2026-06-12 03:01:46 +0300

    Add P2P platform layout

---
+++ src/app/p2p/layout.tsx
@@ -0,0 +1,5 @@
+import { PlatformShell } from '@/components/psp/PlatformShell'
+
+export default function P2PLayout({ children }: { children: React.ReactNode }) {
+  return <PlatformShell>{children}</PlatformShell>
+}
